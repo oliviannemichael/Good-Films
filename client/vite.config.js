0bdcbb5c9f41b3644cfce7a9e1631b52d82680fd
Olivia-Anne-Michael <omichael369@gmail.com>
2023-05-03 12:24:37 +0200

    Initial React commit

---
+++ client/vite.config.js
@@ -0,0 +1,12 @@
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
