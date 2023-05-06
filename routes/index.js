d4ae7d297297c2357ad47a76a1efbfc98aa1de16
Olivia-Anne-Michael <omichael369@gmail.com>
2023-05-06 21:04:02 +0200

    Something's wrong

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const data = require("../model/helper");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' });
 });
+
+// GET movie list from db
+router.get("/", function(req, res, next) {
+  db("SELECT * FROM my_films;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
 
 module.exports = router;
