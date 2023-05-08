fee66ce61d31d2f1babc780c9596dd7802afe09e
Olivia-Anne-Michael <omichael369@gmail.com>
2023-05-08 12:21:11 +0200

    search input and submit button functionality button working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,58 +1,61 @@
 import { useState } from "react";
 import "./App.css";
 
-function searchFilms({ addFilms }) {
-  let [search, setSearch] = useState({ filmId: "" });
+function App() {
+  let [filmId, setFilmId] = useState("");
+  let [movieDetails, setMovieDetails] = useState({});
 
   // input box
   const handleInput = (event) => {
     const name = event.target.name;
     const value = event.target.value;
 
-    setSearch((prevState) => ({ ...prevState, [name]: value }));
+    setFilmId((prevState) => value);
   };
 
   // submit film search button
-  const handleClick = () => {
-    addFilms(search);
+  const handleClick = (event) => {
+    event.preventDefault();
+    getFilms(filmId);
   };
 
   // fetching API
-  const getFilms = () => {
+  const getFilms = (id) => {
     fetch(
       `https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=7c3d5af7d049ad0656688c01e6f47e64`
     )
       .then((response) => response.json())
-      .then((search) => {
-        setSearch(search);
+      .then((data) => {
+        setMovieDetails(data);
+        console.log(data);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  async function addFilms(search) {
-    let options = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(search),
-    };
-    try {
-      let response = await fetch(
-        `https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=7c3d5af7d049ad0656688c01e6f47e64`,
-        options
-      );
-      if (response.ok) {
-        let search = await response.json(); // converts JSON to JavaScript for client/frontend
-        setSearch(search);
-      } else {
-        // server error
-        console.log(`Server error: ${response.status} ${response.statusText}`);
-      }
-    } catch (err) {
-      console.log(`Network error: ${err.message}`);
-    }
-  }
+  // async function addFilms(search) {
+  //   let options = {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(search),
+  //   };
+  //   try {
+  //     let response = await fetch(
+  //       `https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=7c3d5af7d049ad0656688c01e6f47e64`,
+  //       options
+  //     );
+  //     if (response.ok) {
+  //       let search = await response.json(); // converts JSON to JavaScript for client/frontend
+  //       setSearch(search);
+  //     } else {
+  //       // server error
+  //       console.log(`Server error: ${response.status} ${response.statusText}`);
+  //     }
+  //   } catch (err) {
+  //     console.log(`Network error: ${err.message}`);
+  //   }
+  // }
 
   return (
     <div className="main">
@@ -64,7 +67,7 @@ function searchFilms({ addFilms }) {
         <button>My Films</button>
         {/* One input, search by film id */}
         <h2>Search Films</h2>
-        <input type="text" value={search.filmId} onChange={handleInput}></input>
+        <input type="text" value={filmId} onChange={handleInput}></input>
         {/* Button to submit fim id to API and display on page */}
         <button onClick={handleClick}>Go!</button>
       </form>
